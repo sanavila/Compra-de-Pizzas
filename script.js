cb66d7ae8754de7ab5d7f47c07db999a3fa50b32
sanavila <rute.leone@hotmail.com>
2022-07-12 08:49:36 -0300

    Inserção do modal

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ pizzaJson.map((item, index) => {
     let pizzaItem = element('.models .pizza-item').cloneNode(true);
     // preencher as informações em pizzaitem
 
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-    element('.pizza-area').append( pizzaItem );
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=> {
+        e.preventDefault();
+        element('.pizzaWindowArea').style.display = 'flex';
+    })
+
+    element('.pizza-area').append(pizzaItem);
 });
