dfb7b0b61e41ea200d4fe11da8a6c30ceeb4a0f0
sanavila <rute.leone@hotmail.com>
2022-07-14 14:27:35 -0300

    atualização do tamanho dos itens

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const element = (el) => document.querySelector(el);
-const elementAll = (el) => document.querySelector(el);
+const elementAll = (el) => document.querySelectorAll(el);
 
 pizzaJson.map((item, index) => {
     let pizzaItem = element('.models .pizza-item').cloneNode(true);
@@ -11,12 +11,21 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
     pizzaItem.querySelector('a').addEventListener('click', (e)=> {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         element('.pizzaBig img').src = pizzaJson[key].img;
         element('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         element('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        element('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        element('.pizzaInfo--size.selected').classList.remove('selected');
+        elementAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        })
 
         element('.pizzaWindowArea').style.opacity = 0;
         element('.pizzaWindowArea').style.display = 'flex';
